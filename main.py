fbae47c60b348338c493d85a0a59f40f93c4edf4
danielbrownmsm <daniel.brown@msmrockets.org>
2024-02-10 15:43:32 -0600

    remove unused stuff

--- main.py
+++ main.py
@@ -6,19 +6,9 @@ from openai import OpenAI
 # create the client
 client = OpenAI()
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-  ]
-)
-
 personality_msg = {
     "role": "system", # to modify the behavior of the assistant
-    "content": "You are Rosie from The Jetsons"
+    "content": "You are Rosie from The Jetsons."
 }
 
 
