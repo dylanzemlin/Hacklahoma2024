00b4e958e98e507e59d63fd86f99b1bcb1b773bd
danielbrownmsm <daniel.brown@msmrockets.org>
2024-02-10 21:04:29 -0600

    packaged in a class for Dylan

--- main.py
+++ main.py
@@ -1,51 +1,43 @@
 from openai import OpenAI
 import pyttsx3
 
-# https://pypi.org/project/pyttsx3/
-engine = pyttsx3.init() # object creation
-engine.setProperty('rate', 225)     # setting up new voice rate
-engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
-voices = engine.getProperty('voices')       #getting details of current voice
-#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. 0 for male
-engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+class Rosie():
+    def __init__(self):
+        # https://platform.openai.com/docs/guides/text-generation/chat-completions-api
 
-# https://platform.openai.com/docs/guides/text-generation/chat-completions-api
+        # create the client
+        self.client = OpenAI()
+
+        self.personality_msg = {
+            "role": "system", # to modify the behavior of the assistant
+            "content": "You are Rosie from The Jetsons." #this could probably be a lot better
+        }
+
+    
+    def get_response(self, question: str) -> str:
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                self.personality_msg,
+                {"role":"user", "content": question}
+            ]
+        )
+
+        return response.choices[0].message.content # extract ChatGPT's reply
 
 
-# create the client
-client = OpenAI()
+class TTS:
+    def __init__(self):
+        # https://pypi.org/project/pyttsx3/
+        self.engine = pyttsx3.init() # object creation
+        self.engine.setProperty('rate', 225)     # setting up new voice rate
+        self.engine.setProperty('volume', 1.0)    # setting up volume level  between 0 and 1
+        
+        voices = self.engine.getProperty('voices')       #getting details of current voice   
+        for idx, voice in enumerate(voices):
+            if "hazel" in voice.name.lower():
+                self.engine.setProperty('voice', voices[idx].id)
 
-personality_msg = {
-    "role": "system", # to modify the behavior of the assistant
-    "content": "You are Rosie from The Jetsons." #this could probably be a lot better
-}
-
-voices = engine.getProperty('voices')
-for idx, voice in enumerate(voices):
-    if "hazel" in voice.name.lower():
-        engine.setProperty('voice', voices[idx].id)
-
-while True:
-    #TODO get prompts/questions from the google assistant
-    question = input(">")
-
-    if question.lower() == "q":
-        print("EXIT")
-        engine.stop()
-        raise SystemExit
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            personality_msg,
-            {"role":"user", "content":question}
-        ]
-    )
-
-    #TODO pipe this back out to google assistant
-    reply = response.choices[0].message.content # extract ChatGPT's reply
-
-    print(reply)
-
-    engine.say(reply)
-    engine.runAndWait()
+    def speak(self, msg):
+        self.engine.say(msg)
+        self.engine.runAndWait()
