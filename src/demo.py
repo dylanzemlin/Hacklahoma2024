ad8cf1cd84a0c1ca2ed6934f451f76d3f860777e
Dylan Zemlin <dylansmrw@gmail.com>
2024-02-11 09:26:37 -0600

    More demo edits

--- src/demo.py
+++ src/demo.py
@@ -3,19 +3,15 @@ import socket
 import time
 
 HOST = "24.144.83.34"
-PORT = 62431
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-type_message = "MICROPHONE"
-sock.sendall(type_message.encode())
+PORT = 62433
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    type_message = "MICROPHONE"
+    type_message = "RESPONSE"
     sock.sendall(type_message.encode())
     time.sleep(1)
-    sock.sendall(b"DEMO_MSG")
+    sock.sendall(b"Hi, I am Rosie, your personal assistant. How can I help you today?")
 
 
 if __name__ == "__main__":
