5dc72fcf9a75d73d4741b29987319a8016a20476
Dylan Zemlin <dylansmrw@gmail.com>
2024-02-11 03:39:44 -0600

    added try catches to everything so we can not die if something comes unplugged

--- src/rosie/interface.py
+++ src/rosie/interface.py
@@ -16,45 +16,58 @@ class Face(Enum):
 class FaceLcdWriter():
     def __init__(self):
         self.face_serial = serial.Serial(FACE_PORT, BAUD_RATE, timeout=1) # we might need to close this at some point
-        # self.lcd_serial = serial.Serial(LCD_PORT, BAUD_RATE, timeout=1) # same
-
-        # self.close()
+        self.lcd_serial = serial.Serial(LCD_PORT, BAUD_RATE, timeout=1) # same
     
     def open(self):
-        self.face_serial.open()
-        # self.lcd_serial.open()
+        try:
+            self.face_serial.open()
+            self.lcd_serial.open()
 
-        self.face_serial.write(b"fliph") # this is necessary for face specifically to get everything set up
-        time.sleep(0.1)
+            self.face_serial.write(b"fliph") # this is necessary for face specifically to get everything set up
+            time.sleep(0.1)
+        except Exception as e:
+            print(e)
     
     def close(self):
-        self.face_serial.close()
-        # self.lcd_serial.close()
+        try:
+            self.face_serial.close()
+            self.lcd_serial.close()
+        except Exception as e:
+            print(e)
 
     def write_face(self, face):
-        self.face_serial.write(bytes(str(face).removeprefix("Face.").lower() + "\r\n", "utf8"))
+        try:
+            self.face_serial.write(bytes(str(face).removeprefix("Face.").lower() + "\r\n", "utf8"))
+        except Exception as e:
+            print(e)
 
     def write_lcd(self, msg):
-        text = msg.split(" ")
-        output = " ".join(text)
-        chunkSize = 5
-        for i in range(0, len(output), chunkSize):
-            self.lcd_serial.write(bytes(output[i:i+chunkSize if i+chunkSize < len(output) else len(output)], "utf8")) # bad ternary operator to avoid out of bounds error
-            time.sleep(1 * chunkSize)
-        # self.lcd_serial.write(bytes(msg, "utf8"))
+        # text = msg.split(" ")
+        # output = " ".join(text)
+        # chunkSize = 5
+        # for i in range(0, len(output), chunkSize):
+        #     self.lcd_serial.write(bytes(output[i:i+chunkSize if i+chunkSize < len(output) else len(output)], "utf8")) # bad ternary operator to avoid out of bounds error
+        #     time.sleep(1 * chunkSize)
+
+        try:
+            self.lcd_serial.write(bytes(msg, "utf8"))
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     writer = FaceLcdWriter()
+    writer.open()
 
-    time.sleep(3)
+    time.sleep(1)
 
     try:
         writer.open()
     except Exception as e:
         print(e)
 
-    # writer.write_face(Face.HAPPY)
-    # writer.write_lcd("""Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light.""")
-    #writer.write_face(Face.NOTALKFACE)
+    writer.write_face(Face.HAPPY)
+    writer.write_lcd("""Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light.""")
+    time.sleep(5)
+    writer.write_face(Face.NOTALKFACE)
 
     writer.close()
