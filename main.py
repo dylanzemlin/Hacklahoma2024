75a5c041a309823fd82b3cb8e9a100770f49c443
danielbrownmsm <daniel.brown@msmrockets.org>
2024-02-10 20:03:56 -0600

    text to speech taken care of, I think

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from openai import OpenAI
+import pyttsx3
+
+# https://pypi.org/project/pyttsx3/
+engine = pyttsx3.init() # object creation
+engine.setProperty('rate', 225)     # setting up new voice rate
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. 0 for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
 
 # https://platform.openai.com/docs/guides/text-generation/chat-completions-api
 
@@ -11,6 +20,10 @@ personality_msg = {
     "content": "You are Rosie from The Jetsons." #this could probably be a lot better
 }
 
+voices = engine.getProperty('voices')
+for idx, voice in enumerate(voices):
+    if "hazel" in voice.name.lower():
+        engine.setProperty('voice', voices[idx].id)
 
 while True:
     #TODO get prompts/questions from the google assistant
@@ -18,6 +31,7 @@ while True:
 
     if question.lower() == "q":
         print("EXIT")
+        engine.stop()
         raise SystemExit
 
     response = client.chat.completions.create(
@@ -32,3 +46,6 @@ while True:
     reply = response.choices[0].message.content # extract ChatGPT's reply
 
     print(reply)
+
+    engine.say(reply)
+    engine.runAndWait()
